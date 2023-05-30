d3480af4469409992ec80b49537c447509608cf0
Anish Vijay <62013098+iAnishVijay@users.noreply.github.com>
2023-05-30 16:08:18 +0530

    started procedural textures addon

---
+++ procedural-textures/procedural-textures.py
@@ -0,0 +1,16 @@
+bl_info = {
+    "name": "Procedural Textures",
+    "author": "Ani V.",
+    "version": (1, 0),
+    "blender": (2, 80, 0),
+    "location": "View3D > Tool Shelf",
+    "description": "Generates and adds procedural textures like muskgrave, vornoi etc to the selected mesh.",
+    "category": "Material",
+}
+
+import bpy
+
+class OBJECT_PT_procedural_textures(bpy.types.Panel):
+  def execute(self, context):
+        
+    return {'FINISHED'}
